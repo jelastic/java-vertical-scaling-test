74c790a24d34d80d0743f7205ea7b1a03ec49b07
Ruslan Synytsky <siruslan@gmail.com>
2019-09-12 16:31:43 +0300

    small usability improvements

--- src/com/jelastic/verticalscaling/Test.java
+++ src/com/jelastic/verticalscaling/Test.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jelastic.verticalscaling;
 
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  *
@@ -19,19 +12,34 @@ public class Test {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws InterruptedException, IOException {
+        System.out.println("Press <Enter> to start the load test");
         int sleep = 10;
         if (args.length > 0){
             sleep = Integer.parseInt(args[0]);
         }
+        boolean gc = false; 
+        if (args.length > 1){
+            gc = Boolean.parseBoolean(args[1]);
+        }
+        int nGc = 1; 
+        if (args.length > 2){
+            nGc = Integer.parseInt(args[2]);
+        }
         new Thread(new MemoryUsage()).start();
         while (true) {
-            System.in.read();
-            Load test = new Load(sleep);
-            test.run();
-            test = null;
+            int n = System.in.read();
+            if (n > -1){
+                Load test = new Load(sleep);
+                test.run();
+                test = null;
+            }
 
             Thread.sleep(10000);
-            //System.gc();
+            if (gc) 
+                for (int i = 0; i < nGc; i++) {
+                    System.out.println("Calling GC...");
+                    System.gc();
+                }
         }
     }
 
