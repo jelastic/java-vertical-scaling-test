d6a33c6be9523d933eb4f9d9f916caf87e551388
Ruslan Synytsky <siruslan@gmail.com>
2019-09-15 08:47:52 -0700

    + additional testing mode
    based on Gil Tene recommendations

--- src/com/jelastic/verticalscaling/Test.java
+++ src/com/jelastic/verticalscaling/Test.java
@@ -14,32 +14,37 @@ public class Test {
     public static void main(String[] args) throws InterruptedException, IOException {
         System.out.println("Press <Enter> to start the load test");
         int sleep = 10;
-        if (args.length > 0){
+        if (args.length > 0) {
             sleep = Integer.parseInt(args[0]);
         }
-        boolean gc = false; 
-        if (args.length > 1){
-            gc = Boolean.parseBoolean(args[1]);
+        int mode = 1;
+        if (args.length > 1) {
+            mode = Integer.parseInt(args[1]);
         }
-        int nGc = 1; 
-        if (args.length > 2){
-            nGc = Integer.parseInt(args[2]);
+        boolean gc = false;
+        if (args.length > 2) {
+            gc = Boolean.parseBoolean(args[2]);
+        }
+        int nGc = 1;
+        if (args.length > 3) {
+            nGc = Integer.parseInt(args[3]);
         }
         new Thread(new MemoryUsage()).start();
         while (true) {
             int n = System.in.read();
-            if (n > -1){
-                Load test = new Load(sleep);
+            if (n > -1) {
+                Load test = new Load(sleep, mode);
                 test.run();
                 test = null;
             }
 
             Thread.sleep(10000);
-            if (gc) 
+            if (gc) {
                 for (int i = 0; i < nGc; i++) {
                     System.out.println("Calling GC...");
                     System.gc();
                 }
+            }
         }
     }
 
