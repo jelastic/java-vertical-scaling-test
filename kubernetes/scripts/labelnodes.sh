2d25d3cc659a14b4618283c7e443a530c9dfa6d5
bianchi2 <ivantsoft@gmail.com>
2019-10-30 10:02:44 +0200

    Remove epsilon

--- kubernetes/scripts/labelnodes.sh
+++ kubernetes/scripts/labelnodes.sh
@@ -1,6 +1,6 @@
 NODES=$(kubectl get nodes | grep \<none\> | awk '{print $1}')
 NODES=(${NODES// / })
-LABELS=(gc=ZGC gc=Shenandoah gc=C4 gc=OpenJ9 gc=Epsilon gc=G1 gc=G1-dragon)
+LABELS=(gc=ZGC gc=Shenandoah gc=C4 gc=OpenJ9 gc=G1 gc=G1-dragon)
 for i in ${!NODES[*]}; do
   kubectl label nodes ${NODES[$i]} ${LABELS[$i]} --overwrite
 done
